ef63d44bcdda9035ee4f0b150f0d03660fef9264
mks <miso0007@stud.ek.dk>
2026-05-06 07:43:14 +0200

    test controller

--- src/main/java/com/example/connectbe/ConnectBeApplication.java
+++ src/main/java/com/example/connectbe/ConnectBeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConnectBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConnectBeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectBeApplication.class, args);
+    }
 
 }
